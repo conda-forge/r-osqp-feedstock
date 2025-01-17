e19828feba11a3919983f011536860c19241ecdc
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-16 18:35:07 -0800

    fix Rscript path

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
 export DISABLE_AUTOBREW=1
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+    sed -i "s?\$\(R_HOME\)/bin/Rscript?${BUILD_PREFIX}/bin/Rscript?g" src/Makevars
+fi
 ${R} CMD INSTALL --build . ${R_ARGS}
